7e9c9d641c2357f8a7e6961df6842fa2057c8b6a
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2023-09-19 14:52:22 +0400

    Проверка  на пустые значения

--- cookie/component.php
+++ cookie/component.php
@@ -1,12 +1,12 @@
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
-$arResult["BACKGROUND"] = $arParams["BACKGROUND"];
-$arResult["BTN_BACKGROUND"] = $arParams["BTN_BACKGROUND"];
-$arResult["BTN_COLOR"] = $arParams["BTN_COLOR"];
-$arResult["BUTTON"] = $arParams["BUTTON"];
-$arResult["COLOR"] = $arParams["COLOR"];
-$arResult["NAME"] = $arParams["NAME"];
+$arResult["BACKGROUND"] = $arParams["BACKGROUND"] ? $arParams["BACKGROUND"] : "#FFFFFF";
+$arResult["BTN_BACKGROUND"] = $arParams["BTN_BACKGROUND"] ? $arParams["BACKGROUND"] : "#e30613";
+$arResult["BTN_COLOR"] = $arParams["BTN_COLOR"] ? $arParams["BACKGROUND"] : "#FFFFFF";
+$arResult["BUTTON"] = $arParams["BUTTON"] ? $arParams["BACKGROUND"] : "Соглашаюсь";
+$arResult["COLOR"] = $arParams["COLOR"] ? $arParams["BACKGROUND"] : "#FFFFFF";
+$arResult["NAME"] = $arParams["NAME"] ? $arParams["BACKGROUND"] : "Безопасность персональных данных";
 
 $this->IncludeComponentTemplate();
 ?>
