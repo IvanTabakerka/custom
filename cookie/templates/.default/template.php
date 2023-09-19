cba4366a055ed6d64fe5a709cef03e05491ea66d
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2023-09-19 14:49:35 +0400

    Обновление description

--- cookie/templates/.default/template.php
+++ cookie/templates/.default/template.php
@@ -5,7 +5,7 @@
         <p class="cookies__text">
             На сайте используются куки сетевых служб Яндекс и Google. Продолжая просмотр сайта Вы выражаете своё согласие с <a href="/privacy/">Политикой безопасности хранения и обработки персональных данных, разработанной в соответствии с требованиями 152-ФЗ РФ</a> и <a href="/privacy/">Политикой использования файлов куки нашего сайта</a>.
         </p>
-        <a class="btn_custom close-cookes" style="background: <?=$arParams['BTN_BACKGROUND']?>; color:  <?=$arParams['BTN_COLOR']?>;"><?=$arResult['BUTTON']?></a>
+        <a class="btn_custom close-cookes" style="background: <?=$arResult['BTN_BACKGROUND']?>; color:  <?=$arResult['BTN_COLOR']?>;"><?=$arResult['BUTTON']?></a>
     </div>
 </div>
 
