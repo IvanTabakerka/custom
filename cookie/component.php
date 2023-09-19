04ecc48f12cbd3be4b7860a2e667b1c2bddd3ccf
Иван Табакерка <40301518+IvanTabakerka@users.noreply.github.com>
2023-09-19 15:00:05 +0400

    Усиленная проверка

--- cookie/component.php
+++ cookie/component.php
@@ -1,12 +1,23 @@
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
-$arResult["BACKGROUND"] = $arParams["BACKGROUND"] ? $arParams["BACKGROUND"] : "#FFFFFF";
-$arResult["BTN_BACKGROUND"] = $arParams["BTN_BACKGROUND"] ? $arParams["BACKGROUND"] : "#e30613";
-$arResult["BTN_COLOR"] = $arParams["BTN_COLOR"] ? $arParams["BACKGROUND"] : "#FFFFFF";
-$arResult["BUTTON"] = $arParams["BUTTON"] ? $arParams["BACKGROUND"] : "Соглашаюсь";
-$arResult["COLOR"] = $arParams["COLOR"] ? $arParams["BACKGROUND"] : "#FFFFFF";
-$arResult["NAME"] = $arParams["NAME"] ? $arParams["BACKGROUND"] : "Безопасность персональных данных";
+$arResult["BACKGROUND"] = isset($arParams["BACKGROUND"]) ? $arParams["BACKGROUND"] : "#FFFFFF";
+$arResult["BACKGROUND"] = str_replace(["\"","<",">"],"",$arResult["BACKGROUND"]);
+
+$arResult["BTN_BACKGROUND"] = isset($arParams["BTN_BACKGROUND"]) ? $arParams["BTN_BACKGROUND"] : "#e30613";
+$arResult["BTN_BACKGROUND"] = str_replace(["\"","<",">"],"",$arResult["BTN_BACKGROUND"]);
+
+$arResult["BTN_COLOR"] = isset($arParams["BTN_COLOR"]) ? $arParams["BTN_COLOR"] : "#FFFFFF";
+$arResult["BTN_COLOR"] = str_replace(["\"","<",">"],"",$arResult["BTN_COLOR"]);
+
+$arResult["BUTTON"] = isset($arParams["BUTTON"]) ? $arParams["BUTTON"] : "Соглашаюсь";
+$arResult["BUTTON"] = str_replace(["\"","<",">"],"",$arResult["BUTTON"]);
+
+$arResult["COLOR"] = isset($arParams["COLOR"]) ? $arParams["COLOR"] : "#FFFFFF";
+$arResult["COLOR"] = str_replace(["\"","<",">"],"",$arResult["COLOR"]);
+
+$arResult["NAME"] = isset($arParams["NAME"]) ? $arParams["NAME"] : "Безопасность персональных данных";
+$arResult["NAME"] = str_replace(["\"","<",">"],"",$arResult["NAME"]);
 
 $this->IncludeComponentTemplate();
 ?>
